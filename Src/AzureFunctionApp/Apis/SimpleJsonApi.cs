68b694e0e57a31471dfb8cc040df61c47da40f69
Matt Daniels <matt@techtoniq.com>
2021-02-10 11:15:09 +0000

    Skeleton project

---
+++ Src/AzureFunctionApp/Apis/SimpleJsonApi.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+
+namespace GrafanaSimpleJsonDataSourceExample.Apis
+{
+    public class SimpleJsonApi
+    {
+        [FunctionName("test")]
+        public IActionResult Test(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req
+            )
+        {
+            return new OkResult();
+        }
+    }
+}
